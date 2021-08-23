ebbc2a3a7eb5edc56f35b313c2994dda3b03a4a4
Isaac Novaes <isaac.novaes@hotmail.com>
2021-08-23 11:20:37 +0200

    Update form validation

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
-const input = document.getElementById("input-value");
-const inputBorder = document.getElementsByClassName("input-container")[0];
-const errorIcon = document.getElementById("error-icon");
-const errorMessage = document.getElementById("error-message");
+const input = document.getElementById("input-value"),
+	container = document.getElementsByClassName("input-container")[0],
+	errorIcon = document.getElementById("error-icon"),
+	errorMessage = document.getElementById("error-message"),
+	form = document.querySelector("form");
 
 function checkEmail() {
-	if (input.value === "" || input.value === " ") {
-		errorMessage.style.opacity = "1";
-		errorMessage.innerHTML = "Email field is empty";
+	if (input.value.trim() === "") {
+		showError();
+		errorMessage.textContent = "Email field is empty";
 		return false;
+	} else if (
+		!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+			input.value.trim()
+		)
+	) {
+		showError();
+		errorMessage.textContent = "Please provide a valid email";
+		return false;
+	} else {
+		form.innerHTML = `
+			<p class="success">Thank you for sending your email</p>
+		`;
+		return true;
 	}
-	if (!input.value.includes("@")) {
-		return false;
-	}
-	if (input.value.startsWith("@") || input.value.endsWith("@")) {
-		return false;
-	}
-	if (input.value.charAt(input.value.indexOf("@") + 1) == ".") {
-		return false;
-	}
-	if (input.value.startsWith(".") || input.value.endsWith(".")) {
-		return false;
-	}
-	if (input.value.charAt(input.value.indexOf(".") + 1) == ".") {
-		return false;
-	} else return true;
 }
-
+function showError() {
+	errorMessage.style.opacity = "1";
+	errorIcon.style.opacity = "1";
+	container.style.boxShadow = "inset 0 0 0 2px rgb(248, 10, 10)";
+}
 function resetStyle() {
-	inputBorder.style.boxShadow = "none";
+	container.style.boxShadow = "none";
 	errorIcon.style.opacity = "0";
 	errorMessage.style.opacity = "0";
 }
 
 function validateForm() {
 	if (!checkEmail()) {
-		inputBorder.style.boxShadow = "inset 0 0 0 2px rgb(248, 10, 10)";
-		errorIcon.style.opacity = "1";
-		errorMessage.style.opacity = "1";
-		setTimeout(resetStyle, 3000);
-		return false;
+		setTimeout(resetStyle, 3500);
 	}
 }
 
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	validateForm();
+});
